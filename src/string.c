853b9902b66c4c5bd6292c414c5fa7ef23db237e
vSEK1RO <114253920+vSEK1RO@users.noreply.github.com>
2023-10-17 20:10:53 +0300

    uint64_t in string.h

--- src/string.c
+++ src/string.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 
-char * str_init(long long int len)
+char * str_init(uint64_t len)
 {
     char * a=(char *)calloc(sizeof(char),len+1);
     a[len]='\0';
-    for(long long int i=0;i<len;i++){
+    for(uint64_t i=0;i<len;i++){
         a[i]=' ';
     }
     return a;
@@ -13,28 +12,24 @@ char * str_init(long long int len)
 
 char * str_cast(const char * a)
 {   
-    long long int len=sizeof(a)/sizeof(*a);
+    uint64_t len=0;
+    while(a[len]!='\0'){
+        len++;
+    }
     char * b=str_init(len);
-    for(long long int i=0;i<len;i++){
+    for(uint64_t i=0;i<len;i++){
         b[i]=a[i];
     }
     return b;
 }
 
-long long int str_len(char * a)
+uint64_t str_len(char * a)
 {
-    long long int c=0;
-    while(a[c]!='\0'){
-        c++;
+    uint64_t len=0;
+    while(a[len]!='\0'){
+        len++;
     }
-    return c;
-}
-
-void str_print(char * a)
-{
-    for(long long int i=0;i<str_len(a);i++){
-        printf("%c",a[i]);
-    }
+    return len;
 }
 
 void str_del(char * a)
@@ -42,12 +37,12 @@ void str_del(char * a)
     free((void *)a);
 }
 
-long long int str_isEqual(char * a, char * b)
+uint8_t str_isEqual(char * a, char * b)
 {
     if(str_len(a)!=str_len(b)){
         return 0;
     }
-    for(long long int i=0;i<str_len(a);i++){
+    for(uint64_t i=0;i<str_len(a);i++){
         if(a[i]!=b[i]){
             return 0;
         }
@@ -55,7 +50,7 @@ long long int str_isEqual(char * a, char * b)
     return 1;
 }
 
-void str_swap(char * a, long long int i1, long long int i2)
+void str_swap(char * a, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(a);
     i2%=str_len(a);
@@ -64,47 +59,47 @@ void str_swap(char * a, long long int i1, long long int i2)
     a[i1]-=a[i2];
 }
 
-void str_rearr(char * a, long long int i1, long long int i2)
+void str_rearr(char * a, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(a);
     i2%=str_len(a);
     char buff=a[i1];
-    for(long long int i=i1;i>i2;i--){
+    for(uint64_t i=i1;i>i2;i--){
         a[i]=a[i-1];
     }
     a[i2]=buff;
 }
 
-void str_erase(char ** a, long long int i1, long long int i2)
+void str_erase(char ** a, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(*a)+1;
     i2%=str_len(*a)+1;
     char * b=str_init(str_len(*a)-i2+i1);
-    for(long long int i=0;i<i1;i++){
+    for(uint64_t i=0;i<i1;i++){
         b[i]=(*a)[i];
     }
-    for(long long int i=i2;i<str_len(*a);i++){
+    for(uint64_t i=i2;i<str_len(*a);i++){
         b[i+i1-i2]=(*a)[i];
     }
     str_del(*a);
     *a=b;
 }
 
-char * str_substr(char * a, long long int i1, long long int i2)
+char * str_substr(char * a, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(a)+1;
     i2%=str_len(a)+1;
     char * b=str_init(i2-i1);
-    for(long long int i=0;i<i2-i1;i++){
+    for(uint64_t i=0;i<i2-i1;i++){
         b[i]=a[i+i1];
     }
     return b;
 }
 
-long long int str_find(char * a, char key, long long int entry)
+uint64_t str_find(char * a, char key, uint64_t entry)
 {
     entry%=str_len(a)+1;
-    long long int i=-1;
+    uint64_t i=-1;
     while(entry && i!=str_len(a)){
         i++;
         if(a[i]==key){
@@ -114,10 +109,10 @@ long long int str_find(char * a, char key, long long int entry)
     return (entry!=0?-1:i);
 }
 
-long long int str_count(char * a, char n)
+uint64_t str_count(char * a, char n)
 {
-    long long int c=0;
-    for(long long int i=0;i<str_len(a);i++){
+    uint64_t c=0;
+    for(uint64_t i=0;i<str_len(a);i++){
         if(a[i]==n){
             c++;
         }
@@ -125,10 +120,10 @@ long long int str_count(char * a, char n)
     return c;
 }
 
-long long int str_countArr(char * a, char * b)
+uint64_t str_countArr(char * a, char * b)
 {
-    long long int c=0;
-    for(long long int i=0;i<str_len(a)-str_len(b);i++){
+    uint64_t c=0;
+    for(uint64_t i=0;i<str_len(a)-str_len(b);i++){
         if(str_isEqual(str_substr(a,i,i+str_len(b)),b)){
             c++;
         }
@@ -140,7 +135,7 @@ void str_pob(char ** a)
 {   
     if(str_len(*a)!=0){
         char * b=str_init(str_len(*a)-1);
-        for(long long int i=0;i<str_len(*a)-1;i++){
+        for(uint64_t i=0;i<str_len(*a)-1;i++){
             b[i]=(*a)[i];
         }
         str_del(*a);
@@ -151,7 +146,7 @@ void str_pob(char ** a)
 void str_pub(char ** a, char n)
 {
     char * b=str_init(str_len(*a)+1);
-    for(long long int i=0;i<str_len(*a);i++){
+    for(uint64_t i=0;i<str_len(*a);i++){
         b[i]=(*a)[i];
     }
     b[str_len(b)-1]=n;
@@ -163,7 +158,7 @@ void str_pof(char ** a)
 {
     if(str_len(*a)!=0){
         char * b=str_init(str_len(*a)-1);
-        for(long long int i=0;i<str_len(*a)-1;i++){
+        for(uint64_t i=0;i<str_len(*a)-1;i++){
             b[i]=(*a)[i+1];
         }
         str_del(*a);
@@ -174,7 +169,7 @@ void str_pof(char ** a)
 void str_puf(char ** a, char n)
 {
     char * b=str_init(str_len(*a)+1);
-    for(long long int i=0;i<str_len(*a);i++){
+    for(uint64_t i=0;i<str_len(*a);i++){
         b[i+1]=(*a)[i];
     }
     b[0]=n;
@@ -185,29 +180,29 @@ void str_puf(char ** a, char n)
 char * str_concat(char * a, char * b)
 {
     char * c=str_init(str_len(a)+str_len(b));
-    for(long long int i=0;i<str_len(a);i++){
+    for(uint64_t i=0;i<str_len(a);i++){
         c[i]=a[i];
     }
-    for(long long int i=0;i<str_len(b);i++){
+    for(uint64_t i=0;i<str_len(b);i++){
         c[i+str_len(a)]=b[i];
     }
     return c;
 }
 
-void str_ins(char ** a, char n, long long int i)
+void str_ins(char ** a, char n, uint64_t i)
 {
     i%=str_len(*a)+1;
     str_pub(a,n);
     str_rearr(*a,str_len(*a)-1,i);
 }
 
-void str_insArr(char ** a, char * b, long long int i)
+void str_insArr(char ** a, char * b, uint64_t i)
 {
     i%=str_len(*a)+1;
     *a=str_concat(str_concat(str_substr(*a,0,i),b),str_substr(*a,i,str_len(*a)));
 }
 
-void str_replace(char ** a, char n, long long int i1, long long int i2)
+void str_replace(char ** a, char n, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(*a)+1;
     i2%=str_len(*a)+1;
@@ -215,7 +210,7 @@ void str_replace(char ** a, char n, long long int i1, long long int i2)
     str_ins(a,n,i1);
 }
 
-void str_replaceArr(char ** a, char * b, long long int i1, long long int i2)
+void str_replaceArr(char ** a, char * b, uint64_t i1, uint64_t i2)
 {
     i1%=str_len(*a)+1;
     i2%=str_len(*a)+1;
@@ -225,7 +220,7 @@ void str_replaceArr(char ** a, char * b, long long int i1, long long int i2)
 
 void str_reverse(char * a)
 {
-    for(long long int i=0;i<str_len(a)/2;i++){
+    for(uint64_t i=0;i<str_len(a)/2;i++){
         str_swap(a,i,str_len(a)-1-i);
     }
 }
--- src/string.h
+++ src/string.h
@@ -1,28 +1,29 @@
 #ifndef ITERABLE_STRING_H
 #define ITERABLE_STRING_H
 
-char * str_init(long long int len);
+#include <inttypes.h>
+
+char * str_init(uint64_t len);
 char * str_cast(const char * a);
-long long int str_len(char * a);
-void str_print(char * a);
+uint64_t str_len(char * a);
 void str_del(char * a);
-long long int str_isEqual(char * a, char * b);
-void str_swap(char * a, long long int i1, long long int i2);
-void str_rearr(char * a, long long int i1, long long int i2);
-void str_erase(char ** a, long long int i1, long long int i2);
-char * str_substr(char * a, long long int i1, long long int i2);
-long long int str_find(char * a, char key, long long int entry);
-long long int str_count(char * a, char n);
-long long int str_countArr(char * a, char * b);
+uint8_t str_isEqual(char * a, char * b);
+void str_swap(char * a, uint64_t i1, uint64_t i2);
+void str_rearr(char * a, uint64_t i1, uint64_t i2);
+void str_erase(char ** a, uint64_t i1, uint64_t i2);
+char * str_substr(char * a, uint64_t i1, uint64_t i2);
+uint64_t str_find(char * a, char key, uint64_t entry);
+uint64_t str_count(char * a, char n);
+uint64_t str_countArr(char * a, char * b);
 void str_pob(char ** a);
 void str_pub(char ** a, char n);
 void str_pof(char ** a);
 void str_puf(char ** a, char n);
 char * str_concat(char * a, char * b);
-void str_ins(char ** a, char n, long long int i);
-void str_insArr(char ** a, char * b, long long int i);
-void str_replace(char ** a, char n, long long int i1, long long int i2);
-void str_replaceArr(char ** a, char * b, long long int i1, long long int i2);
+void str_ins(char ** a, char n, uint64_t i);
+void str_insArr(char ** a, char * b, uint64_t i);
+void str_replace(char ** a, char n, uint64_t i1, uint64_t i2);
+void str_replaceArr(char ** a, char * b, uint64_t i1, uint64_t i2);
 void str_reverse(char * a);
 
 #include "string.c"
