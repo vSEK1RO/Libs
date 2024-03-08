ab93011480cf3698824048fc801929ee44e3eb23
vSEK1RO <vladlitvinov2006@gmail.com>
2024-03-08 23:04:29 +0300

    mtrx_print and scan int

--- examples/gaussMethod/src/main.c
+++ examples/gaussMethod/src/main.c
@@ -7,19 +7,10 @@ int main()
 {
     mtrx m;
     mtrx_init(&m,2,2,sizeof(int));
-    printf("%lu %lu",mtrx_height(&m),mtrx_width(&m));
-    for(uint64_t i=0;i<mtrx_height(&m);i++){
-        for(uint64_t j=0;j<mtrx_width(&m);j++){
-            scanf("%d",&((int**)m)[i][j]);
-        }
-    }
+    mtrx_scan_int(&m);
     mtrx_mutRow(&m,0,(int[]){2},mtrx_mut_int);
     mtrx_mutRow(&m,1,(int[]){4},mtrx_mut_int);
     mtrx_subRow(&m,0,1,mtrx_sub_int);
-    for(uint64_t i=0;i<mtrx_height(&m);i++){
-        for(uint64_t j=0;j<mtrx_width(&m);j++){
-            printf("%d ",((int**)m)[i][j]);
-        }
-        printf("\n");
-    }
+    mtrx_print_int(&m);
+    mtrx_del(&m);
 }
